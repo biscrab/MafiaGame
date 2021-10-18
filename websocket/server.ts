33d8ea4d9bb7547dbb724f2da8bbc620a448364d
biscrab <biscrab@naver.com>
2021-10-18 15:58:05 +0900

    team

--- websocket/server.ts
+++ websocket/server.ts
@@ -1,28 +1,34 @@
 //server.ts
 
-const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-const port = 5000;
-const app = express();
+import { io } from "socket.io-client";
 
-const server = http.createServer(app);
-const io = socketIO(server);
+const WebSocketS = require("ws").Server;
 
-io.on("connection", (socket) => {
-  socket.on("join", ({ roomName: room, userName: user }) => {
-    socket.join(room);
-    io.to(room).emit("onConnect", `${user} 님이 입장했습니다.`);
-    
-    socket.on("onSend", (messageItem) => {
-      io.to(room).emit("onReceive", messageItem);
+export class Server {
+  public clients: any = [];
+  public wss: any = null;
+  public server: any = null;
+
+  public start(port: number) {
+    this.wss = new WebSocketS({ port: port });  // 내가 설정한 port로 websocket 서버를 연다
+    console.log("WebSocket Initialized", port);
+
+    //웹소켓 연결 핸들러, 연결이 되면 진행됨!
+    this.wss.on("connection", (ws: any) => {
+      this.clients.push(ws);
+      console.log("Connected total:", this.clients.length);
+
+      //메세지 핸들러,클라이언트가 메세지를 보내게되면 여기서 받는다.
+      ws.on("message", (message: string) => {
+        console.log("received: " + message);
+        ws.emit("messageC", message);
+      });
     });
-    
-    socket.on("disconnect", () => {
-      socket.leave(room);
-      io.to(room).emit("onDisconnect", `${user} 님이 퇴장하셨습니다.`);
+    this.wss.on("close", function (error: any) {
+      console.log("websever close", error);
     });
-});
-});
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
+    this.wss.on("error", function (error: any) {
+      console.log(error);
+    });
+  }
+}
--- websocketClient/client/src/App.js
+++ websocketClient/client/src/App.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
-import { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
 import * as S from './styled/App'
+import './App.css'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   const [comment, setComment] = useState({name: "", contents: ""})
 
   let ws = new WebSocket("ws://localhost:1234/ws");
+  let socket = io("http://localhost:1234");
 
   useEffect(()=>{
       ws.onopen = () => {
@@ -16,10 +18,11 @@ function App() {
   },[])
 
   const sendMessage = () => {  // 화살표함수로 만들것!!
-    ws.send("message", comment);  // 서버로 메세지 보내는건 send
-    ws.onmessage = (evt) => {
-      console.log(evt);
-    };
+    if(comment.contents&&comment.name){
+      console.log(comment.contents);
+      ws.send(comment.contents, setComment({...comment, contents:""}))
+      document.body.scrollTop = document.body.scrollHeight;
+    }
   };
 
   const onChange = (e) => {
@@ -27,13 +30,17 @@ function App() {
     setComment({...comment, [name]: value})
   }
 
+  socket.on("message", (message) =>{
+    setItem([...item, message]);
+  });
+
   return (
     <>
       <ul>
         {item.map(
           message => {
             return(
-              <S.Chat>{message}</S.Chat>   
+              <p>{message}</p>   
             )
           })
         }
@@ -41,6 +48,7 @@ function App() {
       <input name="name" onChange={(e)=>onChange(e)} value={comment.name}/>
       <input name="contents" onChange={(e)=>onChange(e)} value={comment.contents}/>
       <button onClick={() => sendMessage()}>메세지 보내기</button>
+      <button onClick={()=>console.log(item)}>item</button>
   </>
   );
 }
